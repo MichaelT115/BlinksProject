9515bc002fcad93b6231046206f1b54bb57a37a3
MichaelT115 <mtc5284@rit.edu>
2020-02-01 19:46:15 -0500

    Add new controls and colors.

--- DamageGame/DamageGame.ino
+++ DamageGame/DamageGame.ino
@@ -1,19 +1,21 @@
-#define ROLE_HULL     0
+#define ROLE_SHIP     0
 #define ROLE_HAZARD   1
-byte _role = ROLE_HULL;
+byte _role = ROLE_SHIP;
+
+//
+// Ship Properties
+//
+#define POINTER_FACE 5
 
 #define MAX_HEALTH 5
 #define START_HEALTH 5
 byte _health = START_HEALTH;
-bool _isAlive = true;
 
 void damageHealth() {
   if (_health == 0)
     return;
 
   --_health;
-
-  _isAlive = _health;
 }
 
 void repairHealth() {
@@ -24,128 +26,128 @@ void repairHealth() {
   }
 }
 
-#define POINTER_FACE 5
+//
+// Hazard Properties
+//
+#define START_OFFSET 0
+byte _hazardOffset = START_OFFSET;
 
-byte _hazardOffset = 0;
+//
+// Communications
+//
+enum messages { 
+  None = 0,     // Placeholder when messages are not being set.
+  Recieved = 1, // A message telling neighbor that their message has been recieved and processed.
+  Damage = 2,   // A message telling neighbor to take damage
+  Repair = 3,   // A message telling neighbor to repair.
+};
+byte messageState[6] = { None, None, None, None, None, None }; 
 
-void setup() {
-}
+bool getNeighborIsAlive(byte face)  { return getLastValueReceivedOnFace(face) >> 5; }
+byte getNeighborMessage(byte face)  { return getLastValueReceivedOnFace(face) & 0b00011111; }
+byte createMessageData(byte face)   { return (_health != 0) << 5 | messageState[face]; }
 
-void loop() {
-  logic();
-
-  display();
-}
-
-// Messages
-enum messages { None, Recieved, Damage, Repair };
-byte messageState[6] = { None, None, None, None, None, None };
-
-bool getNeighborIsAlive(byte face) {
-  return getLastValueReceivedOnFace(face) >> 5;
-}
-
-byte getNeighborMessage(byte face) {
-  return getLastValueReceivedOnFace(face) & 0b00011111;
-}
-
-byte createMessageData(byte face) {
-  return _isAlive << 5 | messageState[face];
-}
-
-void logic() {
-  // Controls
-  if (buttonDoubleClicked()) {
-    if (isAlone()) {
-      _role = _role == ROLE_HULL ? ROLE_HAZARD : ROLE_HULL;
-    }
+//
+// Gameplay
+//
+void gameplay() {
+  // Toggle Role
+  if (buttonLongPressed() && isAlone()) {
+    _role = _role == ROLE_SHIP ? ROLE_HAZARD : ROLE_SHIP;
+    _health = MAX_HEALTH;
   }
 
+  if (buttonMultiClicked()) {
+     _health = MAX_HEALTH;
+  }
+
+  // Trigger Funtion
   if (buttonSingleClicked()) {
     switch (_role) {
-      case ROLE_HULL:
-        if (_isAlive) {
+      case ROLE_SHIP:
+        // Repair pointed at node.
+        if (_health) {
           messageState[POINTER_FACE] = Repair;
           damageHealth();
         }
         break;
       case ROLE_HAZARD:
-        messageState[(_hazardOffset + 0) % FACE_COUNT] = Damage;
-        messageState[(_hazardOffset + 2) % FACE_COUNT] = Damage;
-        messageState[(_hazardOffset + 4) % FACE_COUNT] = Damage;
+        // Damage in three directions.
+        messageState[_hazardOffset + 0] = Damage;
+        messageState[_hazardOffset + 2] = Damage;
+        messageState[_hazardOffset + 4] = Damage;
 
-        _hazardOffset = (_hazardOffset + 1) % FACE_COUNT;
+        // Update hazard directions
+        _hazardOffset = (_hazardOffset + 1) % 2;
         break;
     }
   }
 
   // Listen for messages
-  FOREACH_FACE(f) {
-    switch (messageState[f]) {
-      case None:  // Message is listening
-        standardLoop(f);
+  FOREACH_FACE(face) {
+    // There is no nieghbor, set message to none.
+    if (isValueReceivedOnFaceExpired(face)) {
+       messageState[face] = None;
+       continue;
+    }
+
+    byte message = getNeighborMessage(face);
+    
+    switch (messageState[face]) {
+      case None:  // Face is listening for a message
+        // If there is a valid message.
+        if (message != None && message != Recieved) {
+          handleMessage(face, message);
+        }
         break;
       case Recieved:  // We have recieved a message and are waiting for the channel to clear.
-        listeningLoop(f);
+        if (message == None) {
+          messageState[face] = None;  // Channel is clear. Wait for future message
+        }
         break;
       default:  // We have sent a message and are waiting for confirmation.
-        activeLoop(f);
+        if (message == Recieved) {
+          messageState[face] = None;  // Our message is confirmed. Clear the channel.
+        }
         break;
     }
   }
 
-  // Set messages
+  // Send messages
   FOREACH_FACE(f) {
     setValueSentOnFace(createMessageData(f), f);
   }
 }
 
-void standardLoop(byte face) {
-  // Do we have a neighbor
-  if (isValueReceivedOnFaceExpired(face))
-    return;
-
-  // Did the neighbor send a real message.
-  byte neighborMessage = getNeighborMessage(face);
-  if (neighborMessage == None || neighborMessage == Recieved)
-    return;
-
+void handleMessage(byte face, byte message) {
   // Mark that we recieved the message
   messageState[face] = Recieved;
-
-  // Handle Message
-  switch (neighborMessage) {
+  
+  switch (message) {
     case Damage:
-      if (_role == ROLE_HULL) {
+      // Damage node on opposite side.
+      messageState[(face + 3) % 6] = Damage;
+    
+      if (_role == ROLE_SHIP) {
         damageHealth();
       }
-      messageState[(face + 3) % 6] = Damage;
-      break;
+      return;
     case Repair:
-      repairHealth();
-      break;
+      if (_role == ROLE_SHIP) {
+        repairHealth();
+      }
+      return;
   }
 }
 
-void listeningLoop(byte face) {
-  // The channel is clear.
-  if (isValueReceivedOnFaceExpired(face) || getNeighborMessage(face) == None) {
-    messageState[face] = None;
-  }
-}
-
-void activeLoop(byte face) {
-  // Our message is recieved or there is no recipient
-  if (isValueReceivedOnFaceExpired(face) || getNeighborMessage(face) == Recieved) {
-    messageState[face] = None;
-  }
-}
-
+//
 // Display
+//
+
 void display() {
   switch (_role) {
-    case ROLE_HULL:
-      displayHull();
+    case ROLE_SHIP:
+      displayShip();
       break;
     case ROLE_HAZARD:
       displayHazard();
@@ -155,34 +157,51 @@ void display() {
   }
 }
 
-void displayHull() {
-  byte faceIndex = 0;
-
-  if (!_isAlive) {
-    setColor(OFF);
+#define DEAD_COLOR RED
+#define HEALTH_COLOR GREEN
+#define DAMAGED_COLOR OFF
+#define POINTER_COLOR MAGENTA
+void displayShip() {
+  if (!_health) {
+    setColorOnFace(dim(DEAD_COLOR, 255 * (((millis() - 167 * 0) % 1000) / 1000.0f)), 0);
+    setColorOnFace(dim(DEAD_COLOR, 255 * (((millis() - 167 * 1) % 1000) / 1000.0f)), 1);
+    setColorOnFace(dim(DEAD_COLOR, 255 * (((millis() - 167 * 2) % 1000) / 1000.0f)), 2);
+    setColorOnFace(dim(DEAD_COLOR, 255 * (((millis() - 167 * 3) % 1000) / 1000.0f)), 3);
+    setColorOnFace(dim(DEAD_COLOR, 255 * (((millis() - 167 * 4) % 1000) / 1000.0f)), 4);
+    setColorOnFace(dim(DEAD_COLOR, 255 * (((millis() - 167 * 5) % 1000) / 1000.0f)), 5);    
     return;
   }
+
+  byte faceIndex = 0;
   
   while (faceIndex < _health) {
-    setColorOnFace(GREEN, faceIndex);
+    setColorOnFace(HEALTH_COLOR, faceIndex);
     ++faceIndex;
   }
   while (faceIndex < POINTER_FACE) {
-    setColorOnFace(OFF, faceIndex);
+    setColorOnFace(DAMAGED_COLOR, faceIndex);
     ++faceIndex;
   }
 
-  setColorOnFace(CYAN, POINTER_FACE);
+  setColorOnFace(POINTER_COLOR, POINTER_FACE);
 }
 
-
-#define DAMAGE_COLOR ORANGE
-#define NONDAMAGE_COLOR OFF
+#define HAZARD_DAMAGE_COLOR ORANGE
+#define HAZARD_NONDAMAGE_COLOR OFF
 void displayHazard() {
-  setColorOnFace(DAMAGE_COLOR,    (_hazardOffset + 0) % FACE_COUNT);
-  setColorOnFace(NONDAMAGE_COLOR, (_hazardOffset + 1) % FACE_COUNT);
-  setColorOnFace(DAMAGE_COLOR,    (_hazardOffset + 2) % FACE_COUNT);
-  setColorOnFace(NONDAMAGE_COLOR, (_hazardOffset + 3) % FACE_COUNT);
-  setColorOnFace(DAMAGE_COLOR,    (_hazardOffset + 4) % FACE_COUNT);
-  setColorOnFace(NONDAMAGE_COLOR, (_hazardOffset + 5) % FACE_COUNT);
+  setColorOnFace(HAZARD_DAMAGE_COLOR,    (_hazardOffset + 0));
+  setColorOnFace(HAZARD_NONDAMAGE_COLOR, (_hazardOffset + 1));
+  setColorOnFace(HAZARD_DAMAGE_COLOR,    (_hazardOffset + 2));
+  setColorOnFace(HAZARD_NONDAMAGE_COLOR, (_hazardOffset + 3));
+  setColorOnFace(HAZARD_DAMAGE_COLOR,    (_hazardOffset + 4));
+  setColorOnFace(HAZARD_NONDAMAGE_COLOR, (_hazardOffset + 5) % FACE_COUNT);
+}
+
+// Event Functions:
+
+void setup() { }
+
+void loop() {
+  gameplay();
+  display();
 }
